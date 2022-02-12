76c6b18f233827c7a06447f34d78d9feb9576522
pencilbsp <pencil.bsp@gmail.com>
2022-02-12 09:38:05 +0700

    change glitch port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cors = require('cors')
 const express = require('express')
 const login = require('./controller/login')
 const app = express()
-const port = 3002
+const port = 3000
 
 app.use(cors())
 app.use(express.json())
